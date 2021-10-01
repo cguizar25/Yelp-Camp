5574c29a75ece895d62bc752efff4d4358413105
Christian Guizar <c-guizar@hotmail.com>
2021-10-01 15:41:26 -0700

    style(bootstrap): added basic bootstrap stylings to the crud

--- seeds/index.js
+++ seeds/index.js
@@ -15,17 +15,22 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)]
 
-const seedDb = async () => {
-  await Campground.deleteMany({});
-  for(let i = 0; i < 50; i++){
-    const random1000 = Math.floor(Math.random() * 1000);
-    const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
-    })
-    await camp.save()
-  }
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
 }
+
 seedDb().then(() => {
   mongoose.connection.close();
 })
