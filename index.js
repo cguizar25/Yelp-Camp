03e3e0ffed420241b1aac200f6258c5cace796c4
Christian Guizar <c-guizar@hotmail.com>
2021-11-09 21:43:26 -0800

    feature(authentication): implemented non-specific authorization

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/review')
+const userRoutes = require('./routes/users');
+const campgroundsRoutes = require('./routes/campgrounds')
+const reviewsRoutes = require('./routes/review')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 });
@@ -40,13 +43,26 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
 
-app.use(session(sessionConfig))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/review', reviews)
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/review', reviewsRoutes)
 
 app.get('/', (req, res) => {
   res.render('home');
