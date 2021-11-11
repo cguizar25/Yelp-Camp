b1c33121b1dcc78e5bd2ea74f28a6436311a1f08
Christian Guizar <c-guizar@hotmail.com>
2021-11-11 12:26:22 -0800

    refactor(controllers): added controllers for user and campground routes as weel as star rating

--- middleware.js
+++ middleware.js
@@ -37,14 +37,14 @@ module.exports.isAuthor = async (req, res, next) => {
 };
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-  const { id, reviewId } = req.params;
-  const review = await Review.findById(id);
-  if(!campground.author.equals(req.user._id)) {
-    req.flash('error', 'You do not have permission to do that!');
-    return res.redirect(`/campgrounds/${id}`)
-  }
-  next();
-};
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
 
 module.exports.validateReview = (req, res, next) => {
   // console.log('validate', req);
